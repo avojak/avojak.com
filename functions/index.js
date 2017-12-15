1f656c1cb27ab8bca524427b85a1c40f27944d03
Andrew Vojak <andrew.vojak@gmail.com>
2017-12-15 16:13:57 -0700

    Create partials for index and about pages

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const app = express();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+app.get('/about', function(req, res) {
+  res.render('pages/about');
+});
+
+exports.app = functions.https.onRequest(app);
