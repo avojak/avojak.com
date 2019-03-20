acb3a85cc8fac9489bdbbf1d3892318ed9ed2a48
avojak <andrew.vojak@gmail.com>
2019-03-19 19:02:07 -0600

    Bump p2-inspector version to 2.0.0

--- app.js
+++ app.js
@@ -101,7 +101,7 @@ app.get("/projects/p2-inspector", function(req, res) {
     activeTab: "projects",
     project: {
       name: "p2 Inspector",
-      status: "v1.0.0",
+      status: "v2.0.0",
       description: "A headless Eclipse plugin which exposes a REST interface for inspecting and retrieving the contents of a remote <a href=\"https://www.eclipse.org/equinox/p2/\">p2</a> repository.",
       blogUrl: "https://blog.avojak.com/2019/03/15/p2-inspector/",
       githubUrl: "https://github.com/avojak/p2-inspector",
